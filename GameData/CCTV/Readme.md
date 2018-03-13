64f0fb990756e4ecf7401a5b1aef930ed21a2e66
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-03-13 09:11:29 -0400

    Updated buiold scripts Added jenkins config

---
+++ GameData/CCTV/Readme.md
@@ -0,0 +1,23 @@
+Dynamic CCTV system compatible with KAS (isn't required). Now with WiFi camera
+
+Camera functions:
+
+	Switch on/off
+	Sweeping movement on/off
+	Set FoV (only on controllable craft so set it before you attach it to a building or wreck or whatever)
+	Planned movement detection and zoom
+
+Monitor functions:
+
+	Switch on/off
+	Detect cameras
+	Remote control of camera movement
+	Cycle through attached cameras
+	Best with First Person Eva and KAS Thanks for the guys for the great mods.
+
+Patches are included to work with cameras from the following mods:
+
+	ASET
+	FASA
+	HullcamVDS
+	JSI
--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,20 @@
 
+@echo off
+
+rem H is the destination game folder
+rem GAMEDIR is the name of the mod folder (usually the mod name)
+rem GAMEDATA is the name of the local GameData
+rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
+rem    but not always
 
 set H=R:\KSP_1.3.1_dev
-rem set H=R:\KSP_1.2.2_dev
-echo %H%
+set GAMEDIR=CCTV
+set GAMEDATA="GameData"
+set VERSIONFILE=%GAMEDIR%.version
 
-copy CCTV\bin\Debug\CCTV.dll GameData\CCTV\Plugins
+copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
-xcopy  /E /y /I GameData\CCTV %H%\GameData\CCTV 
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+
+pause
