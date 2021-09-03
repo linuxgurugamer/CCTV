4e7525e71a2eb736a2b15e9075892619c8f909a4
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-09-03 15:00:21 -0400

    Added AssemblyFileVersion Updated version file for 1.12

--- CCTV/AssemblyVersion.cs
+++ CCTV/AssemblyVersion.cs
@@ -5,4 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.1.6.4")]
+ [assembly: AssemblyVersion("1.1.6.5")]
+ [assembly: AssemblyFileVersion("1.1.6.5")]
--- deploy.bat
+++ deploy.bat
@@ -12,7 +12,12 @@ set GAMEDIR=CCTV
 set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
 
+set DP0=r:\dp0\kspdev
+
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
+
 copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
 xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%DP0%\GameData\%GAMEDIR%"
